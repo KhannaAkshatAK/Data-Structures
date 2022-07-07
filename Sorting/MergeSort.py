b597bc4ca148ce34b0a6b5d4f88afcfa1d65837f
KhannaAkshatAK <98173971+KhannaAkshatAK@users.noreply.github.com>
2022-07-07 10:37:37 +0530

    Update MergeSort.py

--- Sorting/MergeSort.py
+++ Sorting/MergeSort.py
@@ -3,9 +3,30 @@ def merge_sort(nums):
     if n<=1:
         return nums
     
-    mid=n//2
-    
+    mid=n//2    
     left = merge_sort(nums[:mid])
     right = merge_sort(nums[mid:])
-    
     return merge_sorted_array(left,right)
+
+
+
+def merge_sorted_array(nums1,nums2):
+    a,b=len(nums1),len(nums2)
+    sorted_list=[]
+    i=j=0
+    
+    while i<a and j<b:
+        if nums1[i]>nums2[j]:
+            sorted_list.append(nums2[j])
+            j+=1
+        else:
+            sorted_list.append(nums1[i])
+            i+=1            
+    while i<a:
+        sorted_list.append(nums1[i])
+        i+=1        
+    while j<b:
+        sorted_list.append(nums2[j])
+        j+=1        
+    return sorted_list
+
