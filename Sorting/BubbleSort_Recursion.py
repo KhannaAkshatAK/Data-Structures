2f9c7c11d87e59588dc15acee03806b6ff44e510
KhannaAkshatAK <98173971+KhannaAkshatAK@users.noreply.github.com>
2022-07-10 22:10:53 +0530

    Update BubbleSort_Recursion.py

--- Sorting/BubbleSort_Recursion.py
+++ Sorting/BubbleSort_Recursion.py
@@ -3,5 +3,5 @@ def bubble_sort(nums):
     for i in range(n-1):
         if nums[i]>nums[i+1]:
             nums[i],nums[i+1]=nums[i+1],nums[i]
-            bubblesort(nums)
+            bubble_sort(nums)
     return nums
